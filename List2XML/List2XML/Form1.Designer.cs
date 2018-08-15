4da2b1a4e54085ea4e593c9a7d802b2c91fafa31
tridip-bba <tridip@bba-reman.com>
2018-08-16 00:06:40 +0530

    2nd one

---
+++ List2XML/List2XML/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace List2XML
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSerialize = new System.Windows.Forms.Button();
+            this.btnDeserialize = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSerialize
+            // 
+            this.btnSerialize.Location = new System.Drawing.Point(94, 12);
+            this.btnSerialize.Name = "btnSerialize";
+            this.btnSerialize.Size = new System.Drawing.Size(75, 23);
+            this.btnSerialize.TabIndex = 0;
+            this.btnSerialize.Text = "Serialize";
+            this.btnSerialize.UseVisualStyleBackColor = true;
+            this.btnSerialize.Click += new System.EventHandler(this.btnSerialize_Click);
+            // 
+            // btnDeserialize
+            // 
+            this.btnDeserialize.Location = new System.Drawing.Point(94, 56);
+            this.btnDeserialize.Name = "btnDeserialize";
+            this.btnDeserialize.Size = new System.Drawing.Size(75, 23);
+            this.btnDeserialize.TabIndex = 1;
+            this.btnDeserialize.Text = "Deserialize";
+            this.btnDeserialize.UseVisualStyleBackColor = true;
+            this.btnDeserialize.Click += new System.EventHandler(this.btnDeserialize_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 111);
+            this.Controls.Add(this.btnDeserialize);
+            this.Controls.Add(this.btnSerialize);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSerialize;
+        private System.Windows.Forms.Button btnDeserialize;
+    }
+}
+
---
+++ List2XML/List2XML/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+using System.Xml;
+using System.IO;
+
+namespace List2XML
+{
+    public partial class Form1 : Form
+    {
+        string _xml = @"<?xml version=""1.0"" ?>
+        <CustomerQueryRs>
+        <CustomerRet>
+        <ListID>9999999bbbb</ListID>
+        <Name>Zlaine Bailey</Name>
+        <FullName>Zlaine Bailey</FullName>
+        <Phone>33333333</Phone>
+        </CustomerRet>
+        <CustomerRet>
+        <ListID>9AAAAAA</ListID>
+        <Name>+ Brian Boyd</Name>
+        <FullName>Brian Boyd</FullName>
+        <Phone>6666666666</Phone>
+        </CustomerRet>
+        <CustomerRet>
+        <ListID>92MMMMMMMMMMM</ListID>
+        <Name>Moni Leahy</Name>
+        <FullName>Moni Leahy</FullName>
+        <Phone>1111111111111</Phone>
+        </CustomerRet>
+        </CustomerQueryRs>";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSerialize_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDeserialize_Click(object sender, EventArgs e)
+        {
+            //XmlDocument doc = new XmlDocument();
+            //doc.LoadXml(_xml);
+            var CustomerList = DeserialzeXml(_xml);
+            //dataGridView1.DataSource = CustomerList;
+        }
+
+        private object DeserialzeXml(string xml)
+        {
+
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(xml);
+            XmlSerializer xs = new XmlSerializer(typeof(CustomerList));
+            CustomerList cl = (CustomerList)xs.Deserialize(new StringReader(doc.OuterXml));
+            return cl.Customers;
+        }
+    }
+
+
+    [XmlRoot("CustomerQueryRs")]
+    public class CustomerList
+    {
+
+        [XmlElement("CustomerRet")]
+        public List<Customer> Customers
+        {
+            get { return customers; }
+            set { customers = value; }
+        }
+
+        private List<Customer> customers = null;
+
+        public CustomerList()
+        {
+            customers = new List<Customer>();
+        }
+    }
+
+    public class Customer
+    {
+        [XmlElement(ElementName = "ListID")]
+        public string ListID { get; set; }
+
+        [XmlElement(ElementName = "Name")]
+        public string Name { get; set; }
+
+        [XmlElement(ElementName = "FullName")]
+        public string FullName { get; set; }
+
+        [XmlElement(ElementName = "Phone")]
+        public string Phone { get; set; }
+    }
+}
